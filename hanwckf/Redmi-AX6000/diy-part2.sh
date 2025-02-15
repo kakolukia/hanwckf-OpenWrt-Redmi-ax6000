953325f80ddadacb163e1d7aa467a306fa117b9d
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-15 14:27:22 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -7,25 +7,25 @@
 #
 # Custom for REDMI AX6000
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-# sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-# sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-# sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-# sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-# sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-# sed -i 's/default-settings-chn/default-settings/g' include/target.mk
-# sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
-# sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
-# sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
-# sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
-# sed -i 's,https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT,https://downloads.openwrt.org/releases/21.02-SNAPSHOT,g' include/version.mk
-# cp $GITHUB_WORKSPACE/hanwckf/Redmi-AX6000/data/mt7986a-xiaomi-redmi-router-ax6000.dtsi target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/
-# cat > package/base-files/files/etc/banner << EOF
-#   _______                     ________        __
-#  |       |.-----.-----.-----.|  |  |  |.----.|  |_
-#  |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
-#  |_______||   __|_____|__|__||________||__|  |____|
-#           |__| W I R E L E S S   F R E E D O M
-#  -----------------------------------------------------
-#  %D %V, %C
-#  -----------------------------------------------------
-# EOF
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/default-settings-chn/default-settings/g' include/target.mk
+sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
+sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
+sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
+sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
+sed -i 's,https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT,https://downloads.openwrt.org/releases/21.02-SNAPSHOT,g' include/version.mk
+cp $GITHUB_WORKSPACE/hanwckf/Redmi-AX6000/data/mt7986a-xiaomi-redmi-router-ax6000.dtsi target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/
+cat > package/base-files/files/etc/banner << EOF
+  _______                     ________        __
+ |       |.-----.-----.-----.|  |  |  |.----.|  |_
+ |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
+ |_______||   __|_____|__|__||________||__|  |____|
+          |__| W I R E L E S S   F R E E D O M
+ -----------------------------------------------------
+ %D %V, %C
+ -----------------------------------------------------
+EOF
