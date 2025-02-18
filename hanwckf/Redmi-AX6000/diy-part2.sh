5a873a0d1d99837a92387ba4f75f354f71060ea9
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-18 18:21:47 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -63,7 +63,7 @@ rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-core
 rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/xray-core
+rm -rf feeds/packages/net/xray-core
 # rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 # rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
