6c27b0695d99794b51b9bd712d4cff32d50a0d0a
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-26 02:20:06 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -62,7 +62,7 @@ rm -rf feeds/luci/applications/luci-app-alist
 # rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
 rm -rf feeds/luci/applications/luci-app-ddns-go
-# rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/alist
 rm -rf feeds/packages/net/cdnspeedtest
 rm -rf feeds/packages/net/chinadns-ng
@@ -81,18 +81,18 @@ rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftabl
 
 # 3rd packages
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-# git clone --depth 1 --single-branch https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 # git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-# git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky && cd package/lucky/ && git checkout 3f856de2d521e1b9b9c6e4f51b24c19a0938f2da && cd -
-# git clone https://github.com/sbwml/luci-app-alist package/alist
-# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git -b js package/luci-app-unblockneteasemusic
-# git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git -b master package/openwrt-cdnspeedtest
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky && cd package/lucky/ && git checkout 3f856de2d521e1b9b9c6e4f51b24c19a0938f2da && cd -
+git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git -b js package/luci-app-unblockneteasemusic
+git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git -b master package/openwrt-cdnspeedtest
 # git clone https://github.com/hubbylei/openwrt-cdnspeedtest -b master package/openwrt-cdnspeedtest
-# git clone https://github.com/hubbylei/luci-app-cloudflarespeedtest -b main package/luci-app-cloudflarespeedtest
+git clone https://github.com/hubbylei/luci-app-cloudflarespeedtest -b main package/luci-app-cloudflarespeedtest
 # git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
 # v2ray-geodata
