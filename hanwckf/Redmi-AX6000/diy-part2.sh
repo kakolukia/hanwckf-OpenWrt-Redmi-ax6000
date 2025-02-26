5abefdcb77148c36d6edb5b30763d44d304ba367
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-27 03:46:28 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -232,4 +232,4 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 ### 更新 feeds ###
 ./scripts/feeds update -a
-./scripts/feeds install -a
+./scripts/feeds install -a -f
