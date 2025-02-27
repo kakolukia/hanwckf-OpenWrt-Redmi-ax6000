067989e3251d4fe316bcc18550efd3aa04b950fb
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-28 00:11:34 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -139,8 +139,7 @@ git clone --depth=1 https://github.com/hubbylei/luci-app-cloudflarespeedtest -b 
 
 
 ### 更新 feeds ###
-./scripts/feeds uninstall -a
-./scripts/feeds install -a
+./scripts/feeds install -a -f
 
 
 ### 程序设置 ###
