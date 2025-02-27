837104d84426330fd261ead4354cc23ab32b037b
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-27 18:23:42 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -95,7 +95,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddn
 # git_sparse_clone main https://github.com/linkease/istore luci
 
 # Lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky && cd package/lucky/ && git checkout 3f856de2d521e1b9b9c6e4f51b24c19a0938f2da && cd -
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky && cd package/lucky/ && git checkout 3f856de2d521e1b9b9c6e4f51b24c19a0938f2da && cd $GITHUB_WORKSPACE
 
 # Koolproxy
 # git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
