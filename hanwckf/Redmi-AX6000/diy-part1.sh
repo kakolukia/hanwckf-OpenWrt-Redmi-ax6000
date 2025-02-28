80731064b67630cfabf55ff7bbe8fd7f5725995b
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-02-28 23:05:16 +0800

    Update diy-part1.sh

--- hanwckf/Redmi-AX6000/diy-part1.sh
+++ hanwckf/Redmi-AX6000/diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
 # echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
 # echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
