e40c03a74edf6b05c8f93fb69834c7e05b241559
kakolukia <30426497+kakolukia@users.noreply.github.com>
2025-03-17 12:27:36 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -173,24 +173,24 @@ git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblocknetea
 # git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 # git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
-# WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
-# mkdir $WORKINGDIR -p
-# rm $WORKINGDIR/* -fr
-# wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
-# unzip $WORKINGDIR/master.zip -d $WORKINGDIR
-# mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
-# rmdir $WORKINGDIR/openwrt-smartdns-master
-# rm $WORKINGDIR/master.zip
-# 
-# LUCIBRANCH="master" #更换此变量
-# WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
-# mkdir $WORKINGDIR -p
-# rm $WORKINGDIR/* -fr
-# wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
-# unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
-# mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
-# rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
-# rm $WORKINGDIR/${LUCIBRANCH}.zip
+WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+mkdir $WORKINGDIR -p
+rm $WORKINGDIR/* -fr
+wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+rmdir $WORKINGDIR/openwrt-smartdns-master
+rm $WORKINGDIR/master.zip
+
+LUCIBRANCH="master" #更换此变量
+WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+mkdir $WORKINGDIR -p
+rm $WORKINGDIR/* -fr
+wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+rm $WORKINGDIR/${LUCIBRANCH}.zip
 
 # Speedtest
 # git clone --depth=1 https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git -b master package/openwrt-cdnspeedtest
